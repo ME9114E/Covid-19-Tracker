b174227d195133812f26e6a232acb4659585e2ac
ME9114E <30830138+ME9114E@users.noreply.github.com>
2020-03-25 16:48:55 +0100

    Add files via upload

---
+++ Corona stats/Corona stats/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Corona_stats
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 11);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 777);
+            this.textBox1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.AutoFitMinFontSize = 5;
+            legend1.IsTextAutoFit = false;
+            legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Column;
+            legend1.Name = "Legend1";
+            legend1.TableStyle = System.Windows.Forms.DataVisualization.Charting.LegendTableStyle.Tall;
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(311, 11);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "cases";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "recovered";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(460, 352);
+            this.chart1.TabIndex = 1;
+            this.chart1.TabStop = false;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            legend2.Title = "Cases per Million";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(311, 389);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series3.CustomProperties = "PieLineColor=Gray, LabelsHorizontalLineSize=0";
+            series3.LabelForeColor = System.Drawing.Color.White;
+            series3.Legend = "Legend1";
+            series3.Name = "cases";
+            this.chart2.Series.Add(series3);
+            this.chart2.Size = new System.Drawing.Size(460, 341);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart2";
+            title1.Name = "Cases per million";
+            title1.Text = "Cases per milion";
+            this.chart2.Titles.Add(title1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 800);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ Corona stats/Corona stats/Form1.cs
@@ -0,0 +1,189 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace Corona_stats
+{
+    public partial class Form1 : Form
+    {
+        private string tot;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string responseBody;
+            List<Dictionary<string, dynamic>> responseBodyJson = new List<Dictionary<string, dynamic>>();
+            Task.Run(async () =>
+            {
+                var response = await client.GetAsync("https://corona.lmao.ninja/countries");
+                responseBody = response.Content.ReadAsStringAsync().Result;
+                responseBodyJson = JsonConvert.DeserializeObject<List<Dictionary<string, dynamic>>>(responseBody);
+            }).Wait();
+
+
+            String url = "https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv";
+            System.Net.WebClient client2 = new System.Net.WebClient();
+            String csv = client2.DownloadString(url);
+
+
+            List<OneCountry> Landen = new List<OneCountry>();
+
+            string[] lines = csv.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            lines = lines.Skip(1).ToArray();
+
+            int ind = new int();
+            DateTime date1 = new DateTime();
+            OneCountry land = new OneCountry();
+            foreach (string line in lines)
+            {
+                string[] entries = line.Split(',');
+
+
+                //check bestaan er al landen
+                //if (Landen.Count == 0)
+                //{
+                //    ind = 0;
+                //    date1 = DateTime.ParseExact(entries[0], "yyyy-MM-dd", null);
+                //}
+                //else
+                //{
+                // check bestaat land al
+                if (line != "")
+                {
+
+
+                    if (land.country == entries[1] && land.provinceOrState == entries[2])
+                    {
+                        // bestaat datum al?
+                        DateTime date2 = DateTime.ParseExact(entries[0], "yyyy-MM-dd", null);
+                        if (date1 != date2)
+                        {
+                            ind++;
+                            date1 = date2;
+                        }
+
+                    }
+                    else
+                    {
+                        if (ind != 0) { Landen.Add(land); }
+                        //nieuw land
+                        land = new OneCountry();
+                        land.country = entries[1];
+                        land.provinceOrState = entries[2];
+                        ind = 0;
+                        date1 = DateTime.ParseExact(entries[0], "yyyy-MM-dd", null);
+                    }
+
+                    //}
+
+                    land.index = ind;
+                    land.addTimestamp(date1);
+                    if (entries[5] != "") { land.addCases(Int32.Parse(entries[5])); }
+                    if (entries[6] != "") { land.addRecovered(Int32.Parse(entries[6])); }
+                    if (entries[7] != "") { land.addDeaths(Int32.Parse(entries[7])); }
+                    
+
+                }
+            }
+
+            // https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv
+
+            //string[] landen = { "Belgium", "Italy", "Spain", "Netherlands" };
+
+            //foreach (string land in landen)
+            //    {
+            //        String url = "https://corona.lmao.ninja/countries/"+land;
+            //        System.Net.WebClient client = new System.Net.WebClient();
+            //        String json = client.DownloadString(url);
+
+            //        var dictionary = JsonConvert.DeserializeObject<IDictionary>(json);
+
+            //        foreach (DictionaryEntry entry in dictionary)
+            //            {
+            //                tot = tot + (entry.Key + ": " + entry.Value) +Environment.NewLine;
+            //            }
+
+            //        tot = tot + Environment.NewLine;
+            //    }
+
+
+
+            foreach (IDictionary dictionary in responseBodyJson)
+            {
+                //    OneCountry land = new OneCountry();
+
+                //    foreach (DictionaryEntry entry in dictionary)
+                //    {
+                //        //tot = tot + (entry.Key + ": " + entry.Value) + Environment.NewLine;
+                //        // int index = land.cases.Length;
+                //        if (entry.Key.Equals("country") && entry.Value != null) { land.country = "" + entry.Value; }
+                //        if (entry.Key.Equals("countryInfo") && entry.Value != null) { land.addFlag("" + entry.Value); }
+                //        if (entry.Key.Equals("cases") && entry.Value != null) { land.addCases(int.Parse("" + entry.Value)); }
+                //        if (entry.Key.Equals("todayCases") && entry.Value != null) { land.addtodayCases(int.Parse("" + entry.Value)); }
+                //        if (entry.Key.Equals("deaths") && entry.Value != null) { land.addDeaths(int.Parse("" + entry.Value)); }
+                //        if (entry.Key.Equals("todayDeaths") && entry.Value != null) { land.addtodayDeaths(int.Parse("" + entry.Value)); }
+                //        if (entry.Key.Equals("recovered") && entry.Value != null) { land.addRecovered(int.Parse("" + entry.Value)); }
+                //        if (entry.Key.Equals("active") && entry.Value != null) { land.addactive(int.Parse("" + entry.Value)); }
+                //        if (entry.Key.Equals("critical") && entry.Value != null) { land.addcritical(int.Parse("" + entry.Value)); }
+                //        if (entry.Key.Equals("casesPerOneMillion") && entry.Value != null) { land.addcasesPerOneMillion(int.Parse("" + entry.Value)); }
+
+                //    }
+                //    Landen.Add(land);
+
+                //}
+                //foreach (OneCountry country in Landen)
+                //{
+
+                //    tot = tot + country.getTimestamp() + Environment.NewLine;
+                //    tot = tot + "country:           " + country.country + Environment.NewLine;
+                //    tot = tot + "cases:             " + country.getCases() + Environment.NewLine;
+                //    tot = tot + "todaycases:        " + country.getTodayCases() + Environment.NewLine;
+                //    tot = tot + "deaths:            " + country.getDeaths() + Environment.NewLine;
+                //    tot = tot + "todaydeaths:       " + country.getTodayDeaths() + Environment.NewLine;
+                //    tot = tot + "recovered:         " + country.getRecoveries() + Environment.NewLine;
+                //    tot = tot + "active:            " + country.getActive() + Environment.NewLine;
+                //    tot = tot + "critical:          " + country.getCritical() + Environment.NewLine;
+                //    tot = tot + "cases per million: " + country.getCasesPerMillion() + Environment.NewLine;
+                //    tot = tot + Environment.NewLine;
+                //}
+                //for (int x = 1; x < 9; x++)
+                //{
+                //    chart1.Series["cases"].Points.AddXY(Landen[x].country, Landen[x].getCases());
+                //    chart1.Series["cases"].Points[x - 1].ToolTip = "" + Landen[x].country + Environment.NewLine + "cases: " + Landen[x].getCases() + Environment.NewLine + "recovered: " + Landen[x].getRecoveries();
+                //    //chart1.Series["cases"].Points[x - 1].Label = "" + Landen[x].getCases();
+                //    chart1.Series["recovered"].Points.AddXY(Landen[x].country, Landen[x].getRecoveries());
+                //    //chart1.Series["recovered"].Points[x - 1].Label= "" + Landen[x].getRecoveries();
+                //    chart1.Series["recovered"].Points[x - 1].ToolTip = "" + Landen[x].country + Environment.NewLine + "cases: " + Landen[x].getCases() + Environment.NewLine + "recovered: " + Landen[x].getRecoveries();
+                //}
+
+                //for (int x = 1; x < 8; x++)
+                //{
+                //    chart2.Series["cases"].Points.AddXY(Landen[x].country, Landen[x].getCasesPerMillion());
+                //    chart2.Series["cases"].Points[x - 1].ToolTip = "" + Landen[x].country + Environment.NewLine + "cases: " + Landen[x].getCases() + Environment.NewLine + "cases per million " + Landen[x].getCasesPerMillion();
+                //}
+                //textBox1.Text = tot;
+            }
+
+
+        }
+    }
+    }
+
+    
+
